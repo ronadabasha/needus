35ab737edc223326561c19c11bc18733071f18c7
Ronada <basharonada@gmail.com>
2024-04-19 10:30:03 +0200

    added inconsolata & inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Fonts from "../fonts";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import clsx from "clsx";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={clsx(Fonts.inconsolata.variable, Fonts.inter.variable)}
+    >
+      <body>{children}</body>
     </html>
   );
 }
